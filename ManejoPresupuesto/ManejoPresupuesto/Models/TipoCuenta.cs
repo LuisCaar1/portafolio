44c140cd9bcb79d9d7ee30a9dd777fc189fc3d71
Luis <luiscarrascoor99rgmail.com>
2023-10-23 19:27:59 +0200

    portafolio

---
+++ ManejoPresupuesto/ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,36 @@
+﻿using ManejoPresupuesto.Validaciones;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using RemoteAttribute = Microsoft.AspNetCore.Mvc.RemoteAttribute;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta //: IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        //[PrimeraLetraMayuscula]
+        [Remote(action: "VerificarExisteTipoCuenta", controller: "TiposCuentas")]
+        public string Nombre { get; set; }
+
+        public int UsuarioId { get; set;}
+
+        public int Orden { get; set;}
+
+       /* public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Nombre != null && Nombre.Length > 0)
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúsucula", 
+                        new[] {nameof(Nombre)});
+                }
+            }
+        }*/
+    }
+}
